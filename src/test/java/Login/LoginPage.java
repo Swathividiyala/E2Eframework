5bbf588359d42fd05d0f586a7ba9969232e12e34
User <swathi.vidiyala@gmail.com>
2021-08-12 20:33:37 +0530

    login update

--- src/test/java/Login/LoginPage.java
+++ src/test/java/Login/LoginPage.java
@@ -2,6 +2,7 @@ package Login;
 
 import java.io.IOException;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import PageObject.BaseClass;
@@ -24,14 +25,30 @@ public void signtoapp()
 {
 	LandingPage ap=new LandingPage(driver);
 	
-	ap.signin().click();;
+	ap.signin().click();
 }
-@Test(priority=2)
-public void getemailpwd()
+
+@Test(dataProvider="getdata", priority=2)
+public void getemailpwd(String username, String password)
 {
-	SigningPage ap=new SigningPage(driver);
-			ap.getemail().sendKeys("swathi");
-			ap.getpassword().sendKeys("vidiyala");
-			ap.getsignin().click();
+	SigningPage ap1=new SigningPage(driver);
+			ap1.getemail().sendKeys(username);
+			ap1.getpassword().sendKeys(password);
+			ap1.getsignin().click();
 }
+@DataProvider
+
+public Object[][] getdata() throws InterruptedException
+{
+	Object[][] data=new Object[2][2];
+	Thread.sleep(3000);
+	data[0][0]="vidiyala@gmail.com";
+	data[0][1]="manasa";
+	Thread.sleep(3000);
+	data[1][0]="vidiyala@gail.com";
+	data[1][1]="vidiyala";
+	return data;
+	
+}
+
 }
